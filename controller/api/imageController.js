8e34407bc7cbf58758a9fd7bef9263415c1390a8
vinhpha111 <vinhpha111@gmail.com>
2019-10-19 09:23:27 +0700

    update

--- controller/api/imageController.js
+++ controller/api/imageController.js
@@ -4,11 +4,17 @@ const sharp = require('sharp');
 var uid = require('uid');
 var fs = require('fs');
 const axios = require('axios');
+var FormData = require('form-data');
 
 module.exports = {
     uploadInEditor: [
         upload.single('upload'),
         async (req, res) => {
+            var dir = './uploads/image/inEditor/resize';
+            if (!fs.existsSync(dir)){
+                fs.mkdirSync(dir);
+            }
+
             let fileName = uid(10);
             await sharp(req.file.path)
             .resize(320)
@@ -17,9 +23,31 @@ module.exports = {
                 res.json({
                     uploaded: 1,
                     fileName: req.file.filename,
-                    url: '/'+'uploads/image/inEditor/resize/'+fileName
+                    url: base_url('uploads/image/inEditor/resize/'+fileName)
                 })
             });
+            
         }
     ]
+}
+
+function uploadToImgBB(imageFile){
+    let key = 'f6bde4da72fa13b0fc0d83a6c3734cd6';
+    let image = base64_encode(imageFile);
+    axios.post('https://api.imgbb.com/1/upload?key='+key+'&image='+image)
+    .then((res)=>{
+        console.log(res);
+        return res.data;
+    })
+    .catch((err)=>{
+        console.log(err);
+        throw err;
+    })
+}
+
+function base64_encode(file) {
+    // read binary data
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
 }
