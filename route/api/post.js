ba1f894d7bf036ee48e0143bc3376d9358f690f2
nguyen vinh pha <pha.nv@neo-lab.vn>
2019-11-05 16:24:17 +0700

    add like post

--- route/api/post.js
+++ route/api/post.js
@@ -4,5 +4,7 @@ var postController = require('../../controller/api/postController')
 
 route.post('/:id/add_comment', postController.addComment);
 route.get('/:id/get_comment', postController.getComment);
+route.post('/:id/set_feel', postController.setFeel);
+route.get('/:id/get_feel', postController.getFeel);
 
 module.exports = route;
