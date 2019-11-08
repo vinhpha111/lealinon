1c2ccfb424bc3f08df722ab2227b1484448db788
vinhpha111 <vinhpha111@gmail.com>
2019-11-09 03:09:44 +0700

    remove directive frontend

--- asset/js/app.directive.js
+++ asset/js/app.directive.js
@@ -116,22 +116,3 @@ app.directive('enterSubmit', function () {
     }
   }
 });
-
-app.directive('clickOutside', function ($document) {
-  return {
-    restrict: 'A',
-    scope: {
-      clickOutside: '&'
-    },
-    link: function (scope, el, attr) {
-      $document.on('click', function (e) {
-        if (el !== e.target && !el[0].contains(e.target)) {
-          scope.$apply(function () {
-            scope.$eval(scope.clickOutside);
-          });
-        }
-      });
-    }
-  }
-
-});
